32149df1064e57aadbd45b97e0614e5ee8cd63d8
Luciano Panepucci <lpanebr@gmail.com>
2024-10-07 18:11:46 -0300

    instalado alguns extras e alguns ajustes harpoon e worktrees

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,34 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- INFO: Fixes harpoon BUG
+-- centering buffer/cursor position when switching
+vim.cmd([[
+" Save current view settings on a per-window, per-buffer basis.
+function! AutoSaveWinView()
+    if !exists("w:SavedBufView")
+        let w:SavedBufView = {}
+    endif
+    let w:SavedBufView[bufnr("%")] = winsaveview()
+endfunction
+
+" Restore current view settings.
+function! AutoRestoreWinView()
+    let buf = bufnr("%")
+    if exists("w:SavedBufView") && has_key(w:SavedBufView, buf)
+        let v = winsaveview()
+        let atStartOfFile = v.lnum == 1 && v.col == 0
+        if atStartOfFile && !&diff
+            call winrestview(w:SavedBufView[buf])
+        endif
+        unlet w:SavedBufView[buf]
+    endif
+endfunction
+
+" When switching buffers, preserve window view.
+if v:version >= 700
+    autocmd BufLeave * call AutoSaveWinView()
+    autocmd BufEnter * call AutoRestoreWinView()
+endif
+]])
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,13 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.g.python3_host_prog = vim.fn.expand("~/.pyenv/versions/neovim3/bin/python")
+
+vim.g.git_worktree = {
+  change_directory_command = "cd",
+  update_on_change = true,
+  update_on_change_command = "e .",
+  clearjumps_on_change = true,
+  confirm_telescope_deletions = true,
+  autopush = false,
+}
