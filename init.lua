25c444ac6fcbcc5acf3a4c526fad6d7a2f771d04
Luciano Panepucci <lpanebr@gmail.com>
2024-10-07 18:12:14 -0300

    configurado lsp para XML lemminx

--- init.lua
+++ init.lua
@@ -1,2 +1,27 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+require("lspconfig").lemminx.setup({
+  settings = {
+    xml = {
+      catalogs = { "/home/lpanebr/catalogs/catalog.xml" },
+      validation = {
+        resolveExternalEntities = true,
+      },
+      completion = {
+        autoCloseTags = true,
+      },
+      format = {
+        splitAttributes = true,
+        joinCDATALines = false,
+        joinContentLines = false,
+        joinCommentLines = false,
+        spaceBeforeEmptyCloseTag = false,
+        enabled = true,
+      },
+      capabilities = {
+        formatting = true,
+      },
+    },
+  },
+})
