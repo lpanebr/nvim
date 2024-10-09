37811f7985fbb069e0f2d47065ad9a09028d2ec8
Luciano Panepucci <lpanebr@gmail.com>
2024-10-09 08:17:14 -0300

    disable ugly annoying flash jumping plugin

---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,19 @@
+return {
+  -- {
+  --   "leap.nvim",
+  --   enabled = false,
+  -- },
+  -- {
+  --   "mini.pairs",
+  --   enabled = false,
+  -- },
+  -- {
+  --   "flit.nvim",
+  --   enabled = false,
+  -- },
+  {
+    "flash.nvim",
+    enabled = false,
+  },
+  -- { "dressing.nvim", enabled = false },
+}
