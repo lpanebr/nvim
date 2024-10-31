445c5d38f0205d78a18590b69f9af25790adfa6f
Luciano Panepucci <lpanebr@gmail.com>
2024-10-31 10:30:39 -0300

    saving...

--- init.lua
+++ init.lua
@@ -25,11 +25,12 @@ require("lspconfig").lemminx.setup({
     },
   },
 })
-local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+
+local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
 parser_config.blade = {
   install_info = {
     url = "https://github.com/EmranMR/tree-sitter-blade", -- local path or git repo
-    files = {"src/parser.c"}, -- note that some parsers also require src/scanner.c or src/scanner.cc
+    files = { "src/parser.c" }, -- note that some parsers also require src/scanner.c or src/scanner.cc
     -- optional entries:
     branch = "main", -- default branch in case of git repo if different from master
     -- generate_requires_npm = false, -- if stand-alone parser without npm dependencies
@@ -38,3 +39,7 @@ parser_config.blade = {
   filetype = "blade", -- if filetype does not match the parser name
 }
 
+-- Tentei deixar o bg das WinBar transparente.
+-- Funciona via comando, mas não por aqui...
+vim.cmd(string.format([[highlight WinBar guibg=%s]], "NONE"))
+vim.cmd(string.format([[highlight WinBarNC guibg=%s]], "NONE"))
