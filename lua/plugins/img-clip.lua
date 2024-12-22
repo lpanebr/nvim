e47d55f8c0cdbe482f4db3565f09b34e5252024b
Luciano Panepucci <lpanebr@gmail.com>
2024-12-22 08:44:53 -0300

    img-clip: não exibir prompt e salvar na mesma pasta.

--- lua/plugins/img-clip.lua
+++ lua/plugins/img-clip.lua
@@ -4,12 +4,15 @@ return {
   opts = {
     default = {
       -- file and directory options
-      dir_path = "imgs", ---@type string | fun(): string
+      dir_path = "", ---@type string | fun(): string
       extension = "png", ---@type string | fun(): string
       file_name = "%Y-%m-%d-%H-%M-%S", ---@type string | fun(): string
       use_absolute_path = false, ---@type boolean | fun(): boolean
       relative_to_current_file = true, ---@type boolean | fun(): boolean
+      prompt_for_file_name = false, ---@type boolean | fun(): boolean
     },
+    -- For project specific settings:
+    -- https://github.com/HakonHarnes/img-clip.nvim?tab=readme-ov-file#project-specific-settings-with-the-img-cliplua-file
   },
   keys = {
     -- suggested keymap
